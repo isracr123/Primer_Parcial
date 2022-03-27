f7f9084a2dd2da317101d43d80db918c222178d0
isracr123 <a01328862@itesm.mx>
2022-03-26 21:20:36 -0600

    full coding api #1

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-/*
- App javascript here
- */
+const input = document.getElementById("places");
+let map;
+let marker;
+let paronama;
+
+function initMap() {
+
+  const Coord = { lat: 42.345573, lng: -71.098326  };
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: Coord,
+  });
+
+  marker = new google.maps.Marker({
+    position: Coord,
+    map: map,
+  });
+  
+  panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"),
+    {
+      position: Coord,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    }
+  );
+
+  map.setStreetView(panorama);
+
+  newAutocomplete();
+}
+
+function newAutocomplete(){
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function(){
+        const place = autocomplete.getPlace();
+        console.log(place);
+        map.setCenter(place.geometry.location);
+        marker.setPosition(place.geometry.location);
+        panorama.setPosition(place.geometry.location);
+    });
+}
+
